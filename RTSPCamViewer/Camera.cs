e3cca9e225bbc6fd4a5bf18ab3ec48631dfb3a1f
ogdemster <ogdemster@gmail.com>
2022-11-14 16:27:14 +0300

    Add project files.

---
+++ RTSPCamViewer/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RTSPCamViewer
+{
+    public class Camera
+    {
+        public int Id { get; set; }
+        public int ParkId { get; set; }
+        public string ParkName { get; set; }
+        public int ParkCameraId { get; set; }
+
+        public string ParkCameraName { get; set; }
+
+        public string ParkCameraUrl { get; set; }
+    }
+}
---
+++ RTSPCamViewer/ParkCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RTSPCamViewer
+{
+    public class ParkCollection
+    {
+
+        public List<Camera> Cameras { get; set; }
+    }
+}
